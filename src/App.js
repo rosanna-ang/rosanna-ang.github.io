bbb4172d14b754d8df6350847cff38e30e65136e
Rosanna Ang <rosanna.c.ang@gmail.com>
2024-10-18 18:01:09 +0800

    read from json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './style/logo.png';
-import fishinmarket from './paintings/fishinmarket.jpg';
+// import fishinmarket from '/paintings/fishinmarket.jpg';
 import './style/style.css';
 import clickImage from "./style/myScript.js";
-import closeImage from "./style/myScript.js";
+import paintingdata from "./data/paintingdata.json";
 
 function App() {
   return (
@@ -31,33 +31,52 @@ function App() {
         </nav>
       </header>
       <body>
-        {/* the modal */}
 
-        <div id="modal01" class="w3-modal" onclick="this.style.display='none'">
-          <div class="w3-modal-content w3-animate-zoom">
-            <h2 class="modal-header">Modal Header</h2>
-            <img id="modalImage" src="" />
-          </div>
+
+
+
+
+        <div>
+          {
+            paintingdata.map((p, index) => {
+
+              return (<div key={index}>
+
+
+
+                {/* the modal */}
+                <div id="modal01" class="w3-modal" onclick="this.style.display='none'">
+                  <div class="w3-modal-content w3-animate-zoom">
+                    <h2 class="modal-header">Modal Header</h2>
+                    <img id="modalImage" src="" />
+                  </div>
+                </div>
+                {/* the painting area */}
+                <div class="flex-container">
+                  <div class="flex-painting">
+                    <div class={p.frame}>
+                    <img src={p.picture}/>
+                    </div>
+                  </div>
+                  <div class="flex-text">
+                    <div class="text-painting">
+                      <p class="painting-title-font">{p.name}</p>
+                      <p>{p.description}</p>
+                      <p>Size</p>
+                      <p>Medium</p>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
+              </div>)
+
+            })
+          }
         </div>
-        {/* the painting area */}
-        <div class="flex-container">
-          <div class="flex-painting">
-            <div class="wood frame2">
-              <img src={fishinmarket} onClick={() => clickImage(fishinmarket)} />
-            </div>
-          </div>
-          <div class="flex-text">
-            <div class="text-painting">
-              <p class="painting-title-font">Fish Market</p>
-              <p>Whereas recognition. In publishing and graphic design, Lorem ipsum is a placeholder text commonly
-                used to demonstrate the visual form of a document a document or a typeface without relying on meaningful
-                content. Whereas recognition. In publishing and graphic design, Lorem ipsum is a placeholder text commonly
-                used to demonstrate the visual form of a document or a typeface without relying on meaningful.</p>
-              <p>Size</p>
-              <p>Medium</p>
-            </div>
-          </div>
-        </div>>
 
       </body>
     </div>
