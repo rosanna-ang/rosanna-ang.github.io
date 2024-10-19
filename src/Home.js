7ba1c68f227d63b38927bd8fc9ad278f3b4ed822
Rosanna Ang <rosanna.c.ang@gmail.com>
2024-10-19 14:25:43 +0800

    reverted out to non carousel

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,26 @@
 
-import paintingview from './style/paintingview.jpg';
 import './style/style.css';
+import ny from './style/ny.jpg';
+import la from './style/la.jpg';
+
 
 
 function Home() {
     return (
         <div className="App">
             <div>
+
+        
+
+
+
                 <p class="home-title-font">Welcome</p>
                 <p class="home-text-font">Artworks are listed for sale in here and here.</p>
+
+        
+
+        
+                
             </div>
 
         </div>
