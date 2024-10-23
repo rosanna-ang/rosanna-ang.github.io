afe0ea85fe038b418fdf1d43bcf9e88c869bcd25
Rosanna Ang <rosanna.c.ang@gmail.com>
2024-10-23 17:45:51 +0800

    about me

--- src/About.js
+++ src/About.js
@@ -8,21 +8,19 @@ function About() {
         <div className="App">
             <div>
                 <div class="wrapper">
-                    <div id="one"class="flex-painting">
+                    <div id="one" class="flex-painting">
                         <div class="no-frame">
                             <img src={paintingview} />
                         </div>
                     </div>
-                    <div id = "two" class="flex-text">
+                    <div id="two" class="flex-text">
                         <div class="text-painting">
-                            <p class="painting-title-font">My name is Rosanna Ang 
+                            <p class="painting-title-font">About
                             </p>
-                            <p>Thank you for visiting my portfolio. </p>
-                    <p>After having explored European landscapes and the female beauty, he experienced some abstract paintings. He rapidly found the pleasure of unlimited creative power. He has really become aware that he has started a never ending journey through imagination. People are now eager to see his new visions !
+                            <p>Rosanna is an amateur artist.  Ever since her childhood she has dabbled in various arts such as painting and music. Having donned the pratical corprate hat for more than 20 years, she is rekindling her passion for acrylic painting by sharing her art to friends, family, and now to the rest of the world as well.
+                    
 
-
-
-Pol Ledent was born in 1952 in Belgium. He came to painting in 1989. He started with watercolor but felt rapidly that oil painting would match his way of being. He is a self-taught painter . Nevertheless he took some drawing lessons in a Belgian academy. After taking part into numerous group exhibitions , some galleries in Belgium proposed to him to exhibit his works. Dinant, Bouillon, Brussels , Paris and Moscow in October 2006. He has already sent many paintings to various countries USA ( New-York Buffalo Broadway ) , Santa Cruz,San Diego, San Jose,Los Angeles, Florida,Miami Baton-Rouge),Seattle, grand blanc (MI), to France, to China, to Scotland, to Portugual, to Switzerland, to Australia and .......... After having explored European landscapes and the female beauty, he experienced some abstract paintings. He rapidly found the pleasure of unlimited creative power. He has really become aware that he has started a never ending journey through imagination. People are now eager to see his new visions</p>
+                            </p>
                         </div>
                     </div>
                 </div>
