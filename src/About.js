1408a2018487fb91afb8832f1940c3868a54c923
Rosanna Ang <rosanna.c.ang@gmail.com>
2024-10-29 15:53:30 +0800

    about me changes

--- src/About.js
+++ src/About.js
@@ -17,7 +17,8 @@ function About() {
                         <div class="text-painting">
                             <p class="painting-title-font">About
                             </p>
-                            <p>Rosanna is an amateur artist.  Ever since her childhood she has dabbled in various arts such as painting and music. Having donned the pratical corprate hat for more than 20 years, she is rekindling her passion for acrylic painting by sharing her art to friends, family, and now to the rest of the world as well.
+                            <p>Rosanna is an amateur artist.  Ever since her childhood she has dabbled in painting and music. 
+                                Having donned the pratical corprate hat for more than 20 years, she is rekindling her passion for art by sharing her paintings to friends, family, and now to the rest of the world as well.
                     
 
                             </p>
